6b1c4fdbf9bb334953adc0c1826071fe9773039a
emidwate <matthewcode04@gmail.com>
2023-06-26 12:47:50 +0200

    Added details to recipeDetails and some styles

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -10,24 +10,32 @@ export default function RecipeDetail() {
     fetch(`/api/recipes/${params.id}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setRecipeData(data);
       });
   }, [params.id]);
 
-  return (
-    <div className='recipe-detail-container'>
-      {recipeData ? (
-        <div className='recipe-detail-wrapper'>
-          <img src={recipeData.recipe.image} className='recipe-detail-img' />
-          <div className='recipe-detail-description'>
-            <h2>{recipeData.recipe.name}</h2>
-            <p>{recipeData.recipe.instructions}</p>
+  if (recipeData) {
+    const ingredients = recipeData.recipe.ingredients.map(
+      (ingredient, index) => <li key={index}>{ingredient}</li>
+    );
+
+    return (
+      <div className='recipe-detail-container'>
+        {recipeData ? (
+          <div className='recipe-detail-wrapper'>
+            <img src={recipeData.recipe.image} className='recipe-detail-img' />
+            <div className='recipe-detail-description'>
+              <h2>{recipeData.recipe.name}</h2>
+              <h4>Preparation time: {recipeData.recipe.preparationTime}</h4>
+              <h5>Ingredients: </h5>
+              <ul>{ingredients}</ul>
+              <p>{recipeData.recipe.instructions}</p>
+            </div>
           </div>
-        </div>
-      ) : (
-        <h2>Loading...</h2>
-      )}
-    </div>
-  );
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
+    );
+  }
 }
